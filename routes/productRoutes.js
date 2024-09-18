8e93fc47d615fc2c9662b508fea9bcaa6d124834
daniel_colina <danielc123avila@icloud.com>
2024-09-18 13:21:26 -0500

    aplicar middleware en productroutes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import upload from "../config/multer.js"
+import adminAccess from "../middleware/adminValidator.js"
 import productController from "../controllers/productController.js";
-
+import productValidation from "../middleware/productsValidator.js"
 const router = express.Router();
 
 router.get("/api/products", productController.getAllProducts);
 router.get("/api/products/:id", productController.getProductById);
-router.post("/api/products", upload.single("avatar"), productController.create);
-router.patch("/api/products/:id", productController.updateProduct);
-router.delete("/api/products/:id", productController.destroy);
+router.post("/api/products", adminAccess, upload.single("avatar"), productValidation.create , productController.create);
+router.patch("/api/products/:id", adminAccess, productController.updateProduct);
+router.delete("/api/products/:id", adminAccess, productController.destroy);
 
 export default router;
